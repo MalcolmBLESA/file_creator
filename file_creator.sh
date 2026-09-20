face7956174e586889a5c21d515990b9700b6ff9
malcolmblesa <malcolmblesa@gmail.com>
2026-09-20 18:12:43 +0200

    you can choose the file name  now

--- file_creator.sh
+++ file_creator.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+if [ $# -ne 1 ]
+then
+    echo "Usage: $0 FILENAME" >&2
+    exit 1
+fi
+
+filename=$1
+
 i=0
 while [ $i -le 10 ]
 do
-    touch test_${i}.txt
-    i=$((i+1))
+    touch ${filename}_${i}.txt
+    i=$((i + 1))
 done
+
